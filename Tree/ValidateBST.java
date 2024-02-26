5d93c120fd90699378463dd4ab22734493550adf
Raghav <raghavamity88@gmail.com>
2024-02-27 00:14:53 +0530

    .

--- Tree/ValidateBST.java
+++ Tree/ValidateBST.java
@@ -11,6 +11,6 @@ public class ValidateBST {
     private boolean isValidBST(TreeNode root, long minVal, long maxVal) {
         if (root == null) return true;
         if (root.data >= maxVal || root.data <= minVal) return false;
-        return isValidBST(root.left, minVal, root.data) && isValidBST(root.right, root.val, maxVal);
+        return isValidBST(root.left, minVal, root.data) && isValidBST(root.right, root.data, maxVal);
     }
 }
