fe9ff8e2b60d2db12cdcab8963b1abf3acf785e4
Raghav <raghavamity88@gmail.com>
2024-02-27 00:13:59 +0530

    .

---
+++ Tree/ValidateBST.java
@@ -0,0 +1,16 @@
+// 98
+// https://leetcode.com/problems/validate-binary-search-tree/description/
+import BinaryTreePack.BinaryTree.TreeNode;;
+
+public class ValidateBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.data >= maxVal || root.data <= minVal) return false;
+        return isValidBST(root.left, minVal, root.data) && isValidBST(root.right, root.val, maxVal);
+    }
+}
